9aa01f098bcdadf2991c7066e4eaec3e2b588f24
Daniel Dominguete <daniel.dominguete@gmail.com>
2020-09-19 19:45:33 -0300

    ready react

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next){
    //middleware adicionando cabeçalhos
    res.header('Access-Control-Allow-Origin', '*')
-   res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT', 'PATCH', 'DELETE')
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
    next()
 }
